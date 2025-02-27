e18328e8425dcc36c6daa01871c4fd1386dedde0
elena5100 <194572363+elena5100@users.noreply.github.com>
2025-02-27 14:48:46 -0800

    Added Submission Rout

--- app.js
+++ app.js
@@ -14,10 +14,20 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.render('home');
 });
+ 
+app.post('submit',(req,res){
 
-
-
-
+    const newPost = {
+        author: req.body.author,
+        title: req.body.title,
+        content: req.body.content
+    };
+    
+    console.log(newPost);
+    
+    res.render('confirmation', { post: newPost });
+});
+    
 
 app.listen(PORT, () => {
     console.log(`Server running http://localhost:${PORT}`);
