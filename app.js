8df51d30c1a02e5f769923163b156deb5b7c895b
elena5100 <194572363+elena5100@users.noreply.github.com>
2025-03-04 15:06:52 -0800

    make changes in app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mariadb.createPool({
-    host: process.env.DB_Host,
-    user: process.env.DB_User,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT
